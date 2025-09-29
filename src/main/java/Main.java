782f2ddc70b8690c99b27512f6bcd137be5648ee
marsiri102 <marsiri102@gmail.com>
2025-09-29 19:00:56 -0400

    Update Main.java
    added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,54 +7,54 @@ public class Main {
 
     public static void main(String[] args) {
         //1
-        printWord(5);
+        printWord(5); //calls the method to print "Hello World" 5 times
 
         //2
-        int result = sumMultiplesof7(10, 30);
+        int result = sumMultiplesof7(10, 30); //calls the method to get the sum of multiples of 7 between 10 and 30
         System.out.println("sum of all numbers between 10 and 30: " + result);
 
         //3
-        int[] sortedArray = {2, 4, 6, 8, 10, 12, 14};
-        int target = 10;
+        int[] sortedArray = {2, 4, 6, 8, 10, 12, 14}; //sorted array
+        int target = 10; //number to look for
 
-        int index = binarySearch(sortedArray, target, 0, sortedArray.length - 1);
+        int index = binarySearch(sortedArray, target, 0, sortedArray.length - 1); //calls the method to look for the target
         System.out.println("Target " + target + " found at index: " + index);
 
     }
     //1
-    public static void printWord(int n) {
+    public static void printWord(int n) { //will print the hello world n amount of times
         if (n <= 0) {
-            return;
+            return; //will stop loop when n is 0 or less than
         }
         System.out.println("Hello World");
-        printWord(n - 1);
+        printWord(n - 1); //loops
     }
     //2
-    public static int sumMultiplesof7(int n1, int n2) {
+    public static int sumMultiplesof7(int n1, int n2) { //return sum of multiple of 7
         if (n1 > n2) {
-            return 0;
+            return 0; //stops
         }
 
         if (n1 % 7 == 0) {
-            return n1 + sumMultiplesof7(n1 + 1, n2);
+            return n1 + sumMultiplesof7(n1 + 1, n2); //adds if it its multiple of 7
         } else {
-            return sumMultiplesof7(n1 + 1, n2);
+            return sumMultiplesof7(n1 + 1, n2); //skips
         }
     }
     //3
-    public static int binarySearch(int[] array, int target, int left, int right) {
+    public static int binarySearch(int[] array, int target, int left, int right) { //binary search
                 if (left > right) {
-                    return -1;
+                    return -1; //Not available
                 }
 
-                int mid = left + (right - left) / 2;
+                int mid = left + (right - left) / 2; //finds middle
 
                 if (array[mid] == target) {
-                    return mid;
+                    return mid;//available 
                 } else if (array[mid] > target) {
-                    return binarySearch(array, target, left, mid - 1);
+                    return binarySearch(array, target, left, mid - 1); //searches left
                 } else {
-                    return binarySearch(array, target, mid + 1, right);
+                    return binarySearch(array, target, mid + 1, right); //searches right
                 }
             }
         }
